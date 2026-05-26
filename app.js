5200a792e8770e6ca53b934934ad507a1b298dc4
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2026-05-26 12:54:22 +0000

    test: full pipline working

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Jenkins + k3s pipeline!\n');
+  res.end('Hello from Jenkins + k3s pipeline! V2\n');
 });
 
 server.listen(3000, () => {
